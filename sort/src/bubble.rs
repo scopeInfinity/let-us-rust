6ce3b92ddf1f13457a01292a96fdc6c3a52a63db
Gagan Kumar <gagan1kumar.cs@gmail.com>
2022-12-24 02:25:27 -0800

    Add unit tests

--- sort/src/bubble.rs
+++ sort/src/bubble.rs
@@ -1,3 +1,10 @@
+/// Bubble sort.
+///
+/// ```rust
+/// let mut arr = [2, 6, 3, 4, 9, -3, 4, 1, 5];
+/// let sorted_arr = sort::bubble::sort(&mut arr);
+/// assert_eq!([-3, 1, 2, 3, 4, 4, 5, 6, 9], sorted_arr);
+/// ```
 pub fn sort(slice: &mut[i32]) -> &mut[i32] {
     let n = slice.len();
     for i in 0..n-1 {
